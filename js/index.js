7c0490d8c1aa66bd612289476e801ae67d02f7b8
yaelBrown <yaelrbrown@gmail.com>
2019-03-25 05:40:11 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// Get browser location
+window.addEventListener('load', ()=> {
+  let long;
+  let lat;
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition( position => {
+      // console.log(position);
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      
+      // CORS proxy to prevent cors errors. 
+      const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+      // darksky.net api call with long and lat
+      // use tilde's with the variables in strings
+      const api = `${proxy}https://api.darksky.net/forecast/3a3c1b32f4117f09303b866e6843c330/${lat},${long}`;
+
+      fetch(api)
+        .then(response =>{
+        return response.json();
+      })
+        .then(data => {
+          console.log(data);
+        });
+    })
+  } else {
+   console.log("It's not working");
+  }
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=wPElVpR1rwA&list=WL&index=8&t=0s
+
+19:57
